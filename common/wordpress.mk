6f34906dcce0ea74f409052bbadce0b7f4008c00
Yuji Iwai <iwai@wovn.io>
2020-11-13 12:26:25 +0900

    Fixed unstable WordPress copy processing.

--- common/wordpress.mk
+++ common/wordpress.mk
@@ -2,7 +2,8 @@
 WP_SITE_TITLE ?= WOVN Example
 WP_PATH       ?= /var/www/html
 WP_LOCAL_PATH ?= public
-WP_SITE_URL   ?= http://127.0.0.1:8080/
+WP_DOMAIN     ?= 127.0.0.1:8080
+WP_SITE_URL   ?= http://$(WP_DOMAIN)/
 WP_LANG_JA    ?= yes
 WP_MULTISITE  ?= no
 
@@ -23,8 +24,8 @@ WP_POPULAR_PLUGIN_LIST    ?= all-in-one-seo-pack \
 	wp-multibyte-patch
 
 WP_SERVICE ?= wordpress
-WP_CLI     ?= docker-compose run --rm wp_cli wp
-WP_SED     ?= docker-compose run --rm $(WP_SERVICE) /bin/sed -i""
+WP_CLI     ?= docker-compose run --rm -w $(WP_PATH) wp_cli wp
+WP_SED     ?= docker-compose run --rm -w $(WP_PATH) $(WP_SERVICE) /bin/sed -i""
 
 
 .PHONY: clean-wordpress
@@ -35,16 +36,15 @@ clean-wordpress:
 
 $(WP_LOCAL_PATH)/index.php:
 	mkdir -p $(WP_LOCAL_PATH)
-	docker-compose run --rm -d $(WP_SERVICE)
-	@echo Waiting for WordPress copying to $(WP_LOCAL_PATH)
-	@sleep 10
-	docker-compose rm -sf $(WP_SERVICE)
+	@echo Initialize for WordPress copying to $(WP_LOCAL_PATH)
+	docker-compose run --rm -w $(WP_PATH) $(WP_SERVICE) apache2 -v
 
 .PHONY: install-wordpress-core
 install-wordpress-core: $(WP_LOCAL_PATH)/index.php
 ifeq ($(WP_MULTISITE),yes)
 	$(WP_CLI) core multisite-install --path=$(WP_PATH) --url="$(WP_SITE_URL)" --title="$(WP_SITE_TITLE)" \
 		--admin_user=$(WP_ADMIN_USER) --admin_password=$(WP_ADMIN_PASS) --admin_email=$(WP_ADMIN_MAIL)
+	$(WP_SED) -e "s/define( 'DOMAIN_CURRENT_SITE', '127.0.0.1:8080' );/# define( 'DOMAIN_CURRENT_SITE', '127.0.0.1:8080' );/" wp-config.php
 else
 	$(WP_CLI) core install --path=$(WP_PATH) --url="$(WP_SITE_URL)" --title="$(WP_SITE_TITLE)" \
 		--admin_user=$(WP_ADMIN_USER) --admin_password=$(WP_ADMIN_PASS) --admin_email=$(WP_ADMIN_MAIL)
@@ -68,5 +68,5 @@ endif
 .PHONY: install-wordpress
 install-wordpress: install-wordpress-core install-wordpress-popular-plugins install-wovnio-plugin
 	@echo Configuring wp-config.php for ngrok
-	$(WP_SED) -e "$$ a ## Automatic configuration addition from Makefile\ndefine('WP_SITEURL', 'http://' . \$$_SERVER['HTTP_HOST']);" wp-config.php
-	$(WP_SED) -e "$$ a ## Automatic configuration addition from Makefile\ndefine('WP_HOME', 'http://' . \$$_SERVER['HTTP_HOST']);" wp-config.php
+	$(WP_SED) -e "/wp-settings.php/i ## Automatic configuration addition from Makefile\ndefine('WP_SITEURL', 'http://' . \$$_SERVER['HTTP_HOST']);" wp-config.php
+	$(WP_SED) -e "/wp-settings.php/i ## Automatic configuration addition from Makefile\ndefine('WP_HOME', 'http://' . \$$_SERVER['HTTP_HOST']);" wp-config.php
