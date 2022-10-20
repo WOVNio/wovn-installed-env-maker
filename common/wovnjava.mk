16bee3ed12c405227956cec3120644466a98c01f
Yuji Iwai <iwai@wovn.io>
2022-10-20 17:31:06 +0900

    Add wovnjava for tomcat9

--- common/wovnjava.mk
+++ common/wovnjava.mk
@@ -1,5 +1,5 @@
 
-WOVNJAVA_VERSION      ?= 1.2.0
+WOVNJAVA_VERSION      ?= 1.14.6
 WOVNJAVA_DOWNLOAD_URL  = https://jitpack.io/com/github/wovnio/wovnjava/$(WOVNJAVA_VERSION)/wovnjava-$(WOVNJAVA_VERSION)-jar-with-dependencies.jar
 TOMCAT_WEBINF          = WEB-INF
 WOVNJAVA_INSTALL_DIR   = $(TOMCAT_WEBINF)/lib
